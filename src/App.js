9f1e60e743ef21a407a0c72e3716c22d11c2ac6b
Gergő Csontos <gergo.csontos.123@gmail.com>
2021-12-06 17:37:48 +0100

    add New Task form toggle to Add button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {useState} from 'react'
 
 
 function App() {
+    const [showAddTask, setShowAddTask] = useState(false)
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -48,9 +49,10 @@ function App() {
     return (
         <>
             <div className="container">
-                <Header />
-                <AddTask onAdd={addTask} />
-                <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+                <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+                : "No tasks to show"}
             </div>
         </>
 
