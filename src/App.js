b3c9e98b612ebcd24603dbbbe52c246279504a4d
Gergő Csontos <gergo.csontos.123@gmail.com>
2021-12-07 12:42:57 +0100

    update project to Router v6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {useState, useEffect} from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
 import Footer from "./components/Footer"
 import About from "./components/About"
+import TaskDetails from "./components/TaskDetails"
 
 function App() {
     const [showAddTask, setShowAddTask] = useState(false)
@@ -81,14 +82,17 @@ function App() {
         <Router>
             <div className="container">
                 <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-                <Route path='/' exact render={(props) => (
-                    <>
-                        {showAddTask && <AddTask onAdd={addTask}/>}
-                        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
-                            : "No tasks to show"}
-                    </>
-                )}/>
-                <Route path='/about' component={About}/>
+                <Routes>
+                    <Route path='/' element={
+                        <>
+                            {showAddTask && <AddTask onAdd={addTask}/>}
+                            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+                                : "No tasks to show"}
+                        </>
+                    }/>
+                    <Route path='/about' element={<About />}/>
+                    <Route path='/task/:id' element={<TaskDetails />}/>
+                </Routes>
                 <Footer/>
             </div>
         </Router>
---
+++ src/components/TaskDetails.js
@@ -0,0 +1,48 @@
+import {useState, useEffect} from 'react'
+import {useParams, useNavigate, useLocation} from "react-router-dom"
+import Button from "./Button"
+
+function TaskDetails() {
+    const [loading, setLoading] = useState(true)
+    const [task, setTask] = useState({})
+    const [error, setError] = useState(null)
+
+    const params = useParams()
+    const navigate = useNavigate()
+    const {pathname} = useLocation()
+
+    useEffect(() => {
+        async function fetchTask() {
+            const response = await fetch(`http://localhost:5000/tasks/${params.id}`)
+            const data = await response.json()
+
+            if (response.status === 404) {
+                setError('Task not found')
+            }
+
+            setTask(data)
+            setLoading(false)
+        }
+        fetchTask()
+    })
+
+    if (error) {
+        navigate('/')
+    }
+
+
+    return loading ? (
+        <h3>Loading...</h3>
+    ) : (
+        <div>
+            <p>{pathname}</p>
+            <h3>{task.text}</h3>
+            <p>{task.day}</p>
+            <Button onClick={() => {
+                navigate(-1)
+            }} text="Go back" />
+        </div>
+    )
+}
+
+export default TaskDetails
